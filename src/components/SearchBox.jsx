2ddfe83814dc090d1e73636d60831b52347c2a60
Remedy Online <agyemangmichael555@gmail.com>
2025-01-05 23:04:10 +0000

    Everyting working very well functionally

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,21 +1,36 @@
+import { IconSearch } from "@tabler/icons-react";
 import PropTypes from "prop-types";
 
 const SearchBox = ({ value, onChange }) => {
-	// console.log(props);
+	const handleSubmit = (e) => {
+		e.preventDefault();
+	};
 
 	return (
 		<section className="w-full flex items-center justify-center">
-			<form className="w-2/3 md:w-3/5 lg:w-1/2 bg-inputBG shadow-lg rounded-full md:py-1 px-5 flex gap-2 items-center">
+			<form
+				role="search"
+				onSubmit={handleSubmit}
+				className="w-2/3 md:w-3/5 lg:w-1/2 bg-inputBG shadow-lg rounded-full md:py-1 px-3 flex gap-2 items-center"
+			>
 				<input
 					type="search"
 					name="search"
-					id="search"
+					id="movie-search"
 					value={value}
 					onChange={onChange}
+					autoComplete="off"
+					aria-label="Search for any movie..."
 					placeholder="Search for any movie..."
-					className="w-full bg-transparent placeholder:text-textColor py-2 outline-none"
+					className="w-full bg-transparent md:text-lg placeholder:text-textColor p-2 outline-none"
 				/>
-				🔍
+				<button
+					type="submit"
+					aria-label="Search"
+					className="hover:bg-cardBG p-2 rounded-full"
+				>
+					<IconSearch className=" w-4 md:w-5 h-4 md:h-5" />
+				</button>
 			</form>
 		</section>
 	);
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,7 @@ const API_URL = "http://www.omdbapi.com?apikey=65b57874";
 const HomePage = () => {
 	const [movies, setMovies] = useState([]);
 	const [searchWord, setSearchWord] = useState("2025");
-	const [favorite, setFavorite] = useState([]);
+	const [favoriteMovies, setFavoriteMovies] = useState([]);
 
 	const fetchMovies = async (searchWord) => {
 		const request = await fetch(`${API_URL}&s=${searchWord}`);
@@ -28,16 +28,36 @@ const HomePage = () => {
 	}, [searchWord]);
 
 	const handleFavorite = (movieID) => {
-		setFavorite((prev) => ({
-			...prev,
-			[movieID]: !prev[movieID],
-		}));
+		const movie = movies.find((m) => m.imdbID === movieID);
+
+		setFavoriteMovies((prev) => {
+			const isMovieInFavorite = prev.some((m) => m.imdbID === movieID);
+
+			if (isMovieInFavorite) {
+				return prev.filter((m) => m.imdbID !== movieID);
+			} else {
+				return [...prev, movie];
+			}
+		});
 	};
+
+	// saving movie to local storage...
+	useEffect(() => {
+		localStorage.setItem("myFavoriteMovies", JSON.stringify(favoriteMovies));
+	}, [favoriteMovies]);
+
+	// retrieving saved movies from local storage...
+	useEffect(() => {
+		const savedMovie = localStorage.getItem("myFavoriteMovies");
+		if (savedMovie) {
+			setFavoriteMovies(JSON.parse(savedMovie));
+		}
+	}, []);
 
 	const renderCTAButtons = (movieID) => {
 		return () => (
 			<MovieCTAbuttons
-				isFavorite={favorite[movieID] || false}
+				isFavorite={favoriteMovies.some((m) => m.imdbID === movieID)}
 				onFavoriteClick={() => handleFavorite(movieID)}
 			/>
 		);
@@ -47,7 +67,7 @@ const HomePage = () => {
 		<section className="h-full mx-auto my-auto items-center justify-start flex flex-col space-y-10 2xl:max-w-7xl text-textColor w-full p-5 ">
 			<div className="flex gap-5">
 				<div className="flex flex-col items-center justify-center">
-					<img src={logoB} alt="logo" className="w-2/3 md:w-full" />
+					<img src={logoB} alt="logo" className="w-2/3 lg:w-full" />
 					<p className="text-sm md:text-xl my-2 italic text-center font-bold">
 						A whole universe of movies at your fingertips
 					</p>
@@ -83,6 +103,34 @@ const HomePage = () => {
 							<p>
 								No movies to display! Start by searching for your favorite
 								movies above.
+							</p>
+						</div>
+					)}
+				</div>
+			</div>
+			<div className="space-y-2 md:space-y-4 w-full">
+				<SectionHeading heading="My Favorite" />
+				<div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-5 ">
+					{favoriteMovies?.length > 0 ? (
+						favoriteMovies.map((movie) => (
+							<MovieCard
+								key={movie.imdbID}
+								// imdbID={movie.imdbID}
+								Poster={movie.Poster}
+								Type={movie.Type}
+								Year={movie.Year}
+								Title={movie.Title}
+								CTAbuttons={renderCTAButtons(movie.imdbID)}
+							/>
+						))
+					) : (
+						<div className="col-span-full text-center py-10 animate-bounce">
+							<p className="text-base md:text-lg font-semibold">
+								Your favorites list is feeling a little lonely...
+							</p>
+							<p className="text-sm md:text-base mt-2">
+								Why not add some blockbuster hits or hidden gems to keep it
+								company?
 							</p>
 						</div>
 					)}
