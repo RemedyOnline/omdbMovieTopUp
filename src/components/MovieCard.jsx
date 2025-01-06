03eb65d6a0af0a6cc7552fd9a122b6256a3120ae
Remedy Online <agyemangmichael555@gmail.com>
2025-01-06 22:57:16 +0000

    All clear bro

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 const MovieCard = ({ imdbID, Poster, Title, Type, Year, CTAbuttons }) => {
 	return (
 		<section className="group py-5 px-2">
-			<div className="flex flex-col text-center bg-cardBG rounded-lg shadow-lg shadow-textColor   justify-start items-center relative max-w-md group-hover:scale-105 transition-all duration-500 ease-in-out overflow-hidden h-[240px] w-[160px] sm:min-w-[200px]">
+			<div className="flex flex-col text-center bg-cardBG rounded-lg shadow-lg shadow-textColor   justify-start items-center relative max-w-md group-hover:scale-105 transition-all duration-500 ease-in-out overflow-hidden h-[240px] sm:h-full w-[160px] sm:min-w-[200px]">
 				<img src={Poster} alt={Title} className="rounded-lg  h-full" />
 				<div className="absolute bottom-0 w-full h-20 bg-gradient-to-t from-cardBG rounded-b-lg to-bgColor text-left px-2 hidden group-hover:block">
 					<p className="hidden">{imdbID}</p>
