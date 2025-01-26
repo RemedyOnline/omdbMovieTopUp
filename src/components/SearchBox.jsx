5ec1ad4442baf143571a89bee7d2b68a7f6f80d7
Remedy Online <agyemangmichael555@gmail.com>
2025-01-26 18:46:17 +0000

    fixed all deployment error due to imported tabler search icon

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,43 +1,57 @@
-import { IconSearch } from "@tabler/icons-react";
 import PropTypes from "prop-types";
 
 const SearchBox = ({ value, onChange }) => {
-	const handleSubmit = (e) => {
-		e.preventDefault();
-	};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
-	return (
-		<section className="w-full flex items-center justify-center sticky top-8 z-10">
-			<form
-				role="search"
-				onSubmit={handleSubmit}
-				className="w-2/3 md:w-3/5 lg:w-1/2 bg-inputBG shadow-lg  rounded-full md:py-1 px-3 flex gap-2 items-center focus:ring focus:ring-red-600"
-			>
-				<input
-					type="search"
-					name="search"
-					id="movie-search"
-					value={value}
-					onChange={onChange}
-					autoComplete="off"
-					aria-label="Search for any movie..."
-					placeholder="Search for any movie..."
-					className="w-full bg-transparent md:text-lg placeholder:text-textColor2 p-2 outline-none"
-				/>
-				<button
-					type="submit"
-					aria-label="Search"
-					className="hover:bg-cardBG p-2 rounded-full"
-				>
-					<IconSearch className=" w-4 md:w-5 h-4 md:h-5" />
-				</button>
-			</form>
-		</section>
-	);
+  return (
+    <section className="sticky top-8 z-10 flex w-full items-center justify-center">
+      <form
+        role="search"
+        onSubmit={handleSubmit}
+        className="flex w-2/3 items-center gap-2 rounded-full bg-inputBG px-3 shadow-lg focus:ring focus:ring-red-600 md:w-3/5 md:py-1 lg:w-1/2"
+      >
+        <input
+          type="search"
+          name="search"
+          id="movie-search"
+          value={value}
+          onChange={onChange}
+          autoComplete="off"
+          aria-label="Search for any movie..."
+          placeholder="Search for any movie..."
+          className="w-full bg-transparent p-2 outline-none placeholder:text-textColor2 md:text-lg"
+        />
+        <button
+          type="submit"
+          aria-label="Search"
+          className="rounded-full p-2 hover:bg-cardBG"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="icon icon-tabler icons-tabler-outline icon-tabler-search"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" />
+            <path d="M21 21l-6 -6" />
+          </svg>
+        </button>
+      </form>
+    </section>
+  );
 };
 
 SearchBox.propTypes = {
-	value: PropTypes.string.isRequired,
-	onChange: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
 };
 export default SearchBox;
